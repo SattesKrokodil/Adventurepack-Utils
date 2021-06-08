a31bf101f922b27115f8f997b802df1089410de5
P03W <pweidman.mail@gmail.com>
2021-06-08 10:19:13 -0400

    Add version check, update properites + fabric.mod.json

--- src/main/java/aputils/ClientAPUtilsMod.java
+++ src/main/java/aputils/ClientAPUtilsMod.java
@@ -6,8 +6,13 @@ import aputils.util.ClientAdvancementChecker;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.networking.v1.ClientLoginNetworking;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.object.builder.v1.client.model.FabricModelPredicateProviderRegistry;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
+
+import java.util.concurrent.CompletableFuture;
 
 @Environment(EnvType.CLIENT)
 public class ClientAPUtilsMod implements ClientModInitializer {
@@ -20,5 +25,11 @@ public class ClientAPUtilsMod implements ClientModInitializer {
                 itemStack.hasTag() && itemStack.getOrCreateSubTag("CellData").contains("Type") && itemStack.getOrCreateSubTag("CellData").getString("Type").equals(cellTypeName)
                 ? 1.0f : 0.0f);
         }
+    
+        ClientLoginNetworking.registerGlobalReceiver(APUtilsMod.versionCheckPacketID, (client, handler, buf, listenerAdder) -> {
+            PacketByteBuf buffer = PacketByteBufs.create();
+            buffer.writeString(APUtilsConfig.getVersion());
+            return CompletableFuture.completedFuture(buffer);
+        });
     }
 }
